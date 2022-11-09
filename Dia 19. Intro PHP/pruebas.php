a747cd28c606f56085859b72dead211d7160f490
Carlos H <98904939+cherediachimeno@users.noreply.github.com>
2022-11-09 13:07:03 +0100

    Add files via upload

---
+++ Dia 19. Intro PHP/pruebas.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>
+        <?php
+
+        $num1 = readline("Dime un número: ");
+        $num2 = readline("Dime otro número: ");
+        echo "El resultado de la suma es ".($num1+$num2)
+
+            /*
+        $name = readline("Your name: ");
+        $int = readline('Enter an integer: ');
+        $float = readline('Enter a floating'
+            . ' point number: ');
+
+        echo "Hello " . $name . " The integer value you entered is "
+            . $int
+            . " and the float value is " . $float; */
+
+        /* echo '<div style="background: grey; widht: 50px; height: 50px">
+         Mis primeras líneas en PHP!
+         </div>'
+         // Comentarios en línea
+         /* 
+            Comentarios en bloque
+         */
+        // let numero = 1; numero = 50;
+        /*
+         $num1 = 1;
+         $num2 = 20;
+
+         echo "num1 es $num1 y num2 es $num2</br>";
+         $numero_entero = 3;
+
+         $mis_colores = "azul";
+         $misColores = "rojo"; 
+
+         $nombre = "Carlos";
+
+         echo "Hola $nombre</br>";
+         echo "Hola ". $nombre; // "hola" + nombre en JS*/
+        ?>
+
+    </p>
+    <p>
+        <?php
+        echo "Mis segundas \"líneas\" en PHP!"
+        ?>
+    </p>
+
+</body>
+
+</html>
